83df162bed394f749cb49a65343e729622ec9143
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-12-24 13:57:32 -0500

    use the API rather than raw strings

--- HapticGloveFirmware/HapticGloveFirmware.ino/HapticGloveFirmware.ino.ino
+++ HapticGloveFirmware/HapticGloveFirmware.ino/HapticGloveFirmware.ino.ino
@@ -1,30 +1,22 @@
-/*********************************************************************
- This is an example for our nRF51822 based Bluefruit LE modules
-
- Pick one up today in the adafruit shop!
-
- Adafruit invests time and resources providing this open source code,
- please support Adafruit and open-source hardware by purchasing
- products from Adafruit!
-
- MIT license, check LICENSE for more information
- All text above, and the splash screen below must be included in
- any redistribution
-*********************************************************************/
-
 #include <Arduino.h>
 #include <SPI.h>
 #if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
   #include <SoftwareSerial.h>
+  #define WTF 1
+#else
+  #define WTF 0
 #endif
 
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
-#include "Adafruit_BluefruitLE_UART.h"
+#include "Adafruit_BLEGatt.h"
 
 #include "BluefruitConfig.h"
 #define FACTORYRESET_ENABLE 1
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+Adafruit_BLEGatt gatt(ble);
+
+uint8_t serviceID, char1ID, char2ID;
 
 void stop(){
   while(1);
@@ -44,14 +36,15 @@ void setup(void)
     stop();
   }
   ble.echo(false);
-  atcommand(F("AT+FACTORYRESET"), 1000)  // Wait 1s for this to complete
-  atcommand(F("AT+GATTCLEAR"));
-  atcommand(F("AT+GATTADDSERVICE=UUID=0x180D"));
-  atcommand(F("AT+GATTADDCHAR=UUID=0x2A37, PROPERTIES=0x10, MIN_LEN=2, MAX_LEN=3, VALUE=00-40"));
-  atcommand(F("AT+GATTADDCHAR=UUID=0x2A38, PROPERTIES=0x02, MIN_LEN=1, VALUE=3"));
-  atcommand(F("AT+GAPSETADVDATA=02-01-06-05-02-0d-18-0a-18"));
-  // Perform a system reset and wait 1s to come back online
-  atcommand(F("ATZ"), 1000);
+  ble.factoryReset(true);
+  gatt.clear();
+  serviceID = gatt.addService(0x180D);
+  char1ID = gatt.addCharacteristic(0x2A37, 0x10, 2, 3, BLE_DATATYPE_BYTEARRAY);
+  char2ID = gatt.addCharacteristic(0x2A38, 0x02, 1, 1, BLE_DATATYPE_INTEGER);
+  gatt.setChar(char1ID, {0x00, 0x40}, 2);
+  gatt.setChar(char2ID, 3);
+  ble.setAdvData({0x02, 0x01, 0x06, 0x05, 0x02, 0x0d, 0x18, 0x0a, 0x18}, 9);
+  ble.reset(true);
 }
 
 void loop(void)

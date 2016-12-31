dd734dc7a9771bf9cc889edbb4d6075413fc414f
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-12-31 09:16:21 -0500

    stow

---
+++ HapticGloveServer/Glove.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Bluetooth;
+
+namespace HapticGloveServer
+{
+    public class Glove
+    {
+        public Glove(DeviceInformation device)
+        {
+        }
+    }
+}

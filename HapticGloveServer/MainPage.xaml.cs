d3270d3be9b240dc9063bcccb1378d11c1dd0c9e
Sean T. McBeth <sean.mcbeth@gmail.com>
2017-01-23 18:24:23 -0500

    only print if things change

--- HapticGloveServer/MainPage.xaml.cs
+++ HapticGloveServer/MainPage.xaml.cs
@@ -1,99 +1,108 @@
 ﻿using System;
 using Windows.UI.Xaml.Controls;
 using System.Threading;
+using System.Text;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace HapticGloveServer
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    Timer t;
+
+    public MainPage()
     {
-        Timer t;
+      this.InitializeComponent();
+      t = new Timer(Tick, this, 3000, 1000);
+    }
 
-        public MainPage()
+    private HapticGlove.GloveState lastState = HapticGlove.GloveState.NotReady;
+    private StringBuilder sb = new StringBuilder();
+
+    private void Tick(object state)
+    {
+      var glove = HapticGlove.Glove.DEFAULT;
+      if(glove != null)
+      {
+        if(!glove.State.HasFlag(HapticGlove.GloveState.Ready))
         {
-            this.InitializeComponent();
-            t = new Timer(Tick, this, 3000, 1000);
+          if(glove.State != lastState)
+          {
+            lastState = glove.State;
+            WriteLine(glove.State.ToString());
+          }
+
+          if(!glove.State.HasFlag(HapticGlove.GloveState.Searching) && glove.State.HasFlag(HapticGlove.GloveState.DeviceFound))
+          {
+            glove.Connect();
+          }
         }
-
-        private HapticGlove.GloveState lastState = HapticGlove.GloveState.NotReady;
-
-        private void Tick(object state)
+        else
         {
-            var glove = HapticGlove.Glove.DEFAULT;
-            if(glove != null)
-            {
-                if(!glove.State.HasFlag(HapticGlove.GloveState.Ready))
-                {
-                    if(glove.State != lastState)
-                    {
-                        lastState = glove.State;
-                        WriteLine(glove.State.ToString());
-                    }
+          sb.Clear();
+          sb.AppendFormat("Finger [Battery: {0}%, Fingers: ",
+              glove.Battery,
+              glove.State == HapticGlove.GloveState.Ready ? "Ready" : glove.State.ToString(),
+              glove.Fingers.Count);
+          for(int i = 0; i < glove.Fingers.Count; ++i)
+          {
+            sb.AppendFormat(", {0}", glove.Fingers[i]);
+          }
+          sb.AppendFormat(" Motors: {0}", glove.Motors.Count);
+          for(int i = 0; i < glove.Motors.Count; ++i)
+          {
+            sb.AppendFormat(", {0}", glove.Motors[i]);
+          }
+          sb.AppendFormat("]");
 
-                    if(!glove.State.HasFlag(HapticGlove.GloveState.Searching) && glove.State.HasFlag(HapticGlove.GloveState.DeviceFound))
-                    {
-                        glove.Connect();
-                    }
-                }
-                else
-                {
-                    Write("Finger [Battery: {0}%, Fingers: ",
-                        glove.Battery,
-                        glove.State == HapticGlove.GloveState.Ready ? "Ready" : glove.State.ToString(),
-                        glove.Fingers.Count);
-                    for(int i = 0; i < glove.Fingers.Count; ++i)
-                    {
-                        Write(", {0}", glove.Fingers[i]);
-                    }
-                    Write(" Motors: {0}", glove.Motors.Count);
-                    for(int i = 0; i < glove.Motors.Count; ++i)
-                    {
-                        Write(", {0}", glove.Motors[i]);
-                    }
-                    Write("]");
-
-                    if(glove.Error != null)
-                    {
-                        Write(" ERROR: {0}", glove.Error.Message);
-                        t.Change(Timeout.Infinite, Timeout.Infinite);
-                    }
-                    WriteLine();
-                }
-            }
-        }
-
-        private void Write(string msg = "")
-        {
-            this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-            {
-                this.Description += msg;
-                this.Bindings.Update();
-            }).AsTask().Wait();
-        }
-
-        private void WriteLine(string msg = "")
-        {
-            Write(msg + "\n");
+          if(glove.Error != null)
+          {
+            sb.AppendFormat(" ERROR: {0}", glove.Error.Message);
+            t.Change(Timeout.Infinite, Timeout.Infinite);
+          }
+          WriteLine(sb.ToString());
           glove.Motors.Test();
         }
-
-        private void Write(string format, params object[] args)
-        {
-            Write(string.Format(format, args));
-        }
-
-        private void WriteLine(string format, params object[] args)
-        {
-            WriteLine(string.Format(format, args));
-        }
-
-        public string Description
-        {
-            get; set;
-        }
+      }
     }
+
+    private string lastMessage;
+
+    private void Write(string msg = "")
+    {
+      if(msg != lastMessage)
+      {
+        lastMessage = msg;
+        this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+        {
+          this.Description += msg;
+          this.Bindings.Update();
+        }).AsTask().Wait();
+      }
+    }
+
+    private void WriteLine(string msg = "")
+    {
+      Write(msg + "\n");
+    }
+
+    private void Write(string format, params object[] args)
+    {
+      Write(string.Format(format, args));
+    }
+
+    private void WriteLine(string format, params object[] args)
+    {
+      WriteLine(string.Format(format, args));
+    }
+
+    public string Description
+    {
+      get; set;
+    }
+  }
 }

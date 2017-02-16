dfdb90b98d0f4e9ed2647a80ec06acc64a0952fa
Sean T. McBeth <sean.mcbeth@gmail.com>
2017-02-16 13:29:23 -0500

    latest GloveComponent
    This won't work with the test project here, but it will work with the real LaserGuantlet app, but that app is 8GB is size and I need a way to share the code with David, so it's going in here.

--- Haptic Glove - Test - Unity/Assets/GloveComponent.cs
+++ Haptic Glove - Test - Unity/Assets/GloveComponent.cs
@@ -7,8 +7,15 @@ using System.Linq;
 
 public class GloveComponent : MonoBehaviour
 {
-    public GameObject[] fingers;
+
+    public Animation[] fingers;
     public bool[] motors = new bool[5];
+
+    private float[] lastFingerValues;
+
+    private float thresholdValue = 0.3f;
+
+    public GauntletController gauntletController;
 
     TcpClient server;
     Stream stream;
@@ -21,10 +28,15 @@ public class GloveComponent : MonoBehaviour
     void Start()
     {
         q = new Queue<byte>();
-        fingers = (from o in FindObjectsOfType<GameObject>()
-                  where o.name.StartsWith("Finger")
-                  orderby o.name
-                  select o).ToArray();
+
+        lastFingerValues = new float[5];
+
+        fingers[0].Stop();
+        fingers[1].Stop();
+        fingers[2].Stop();
+        fingers[3].Stop();
+        fingers[4].Stop();
+
     }
 
     // Update is called once per frame
@@ -32,60 +44,94 @@ public class GloveComponent : MonoBehaviour
     {
         if(this.server == null)
         {
-            this.server = new TcpClient();
-            this.server.Connect("127.0.0.1", 9001);
-            this.stream = this.server.GetStream();
+            ConnectToServer();
         }
         else if(this.server.Connected)
         {
-            while(this.server.Available > 0)
-            {
-                var b = (byte)this.stream.ReadByte();
-                if(b < byte.MaxValue)
-                {
-                    q.Enqueue(b);
-                }
-                else if(q.Count >= 2)
-                {
-                    while(q.Count > 2)
-                    {
-                        q.Dequeue();
-                    }
-                    int i = q.Dequeue();
-                    float y = q.Dequeue() / 255f;
-                    if(0 <= i && i < this.fingers.Length)
-                    {
-                        var f = this.fingers[i];
-                        var s = f.transform.localScale;
-                        s.y = y;
-                        f.transform.localScale = s;
-                        s = f.transform.localPosition;
-                        s.y = y;
-                        f.transform.localPosition = s;
-                    }
-                }
-            }
-
-            byte state = 0;
-            for(int i = 0; i < this.motors.Length; ++i)
-            {
-                state <<= 1;
-                if(this.motors[i])
-                {
-                    state |= 1;
-                }
-            }
-            if(state != lastState)
-            {
-                lastState = temp[0] = state;
-                this.stream.Write(temp, 0, 1);
-            }
+            UpdateServer();
         }
         else
         {
-            this.stream.Dispose();
-            this.stream = null;
-            this.server = null;
+            DisconnectFromServer();
+        }
+    }
+
+    private void DisconnectFromServer()
+    {
+        this.stream.Dispose();
+        this.stream = null;
+        this.server = null;
+    }
+
+    private void UpdateServer()
+    {
+        while (this.server.Available > 0)
+        {
+            var b = (byte)this.stream.ReadByte();
+            if (b < byte.MaxValue)
+            {
+                q.Enqueue(b);
+            }
+            else if (q.Count >= 2)
+            {
+                while (q.Count > 2)
+                {
+                    q.Dequeue();
+                }
+                int i = q.Dequeue();
+                float y = q.Dequeue() / 255f;
+                UpdateFinger(i, y);
+            }
+        }
+
+        byte state = 0;
+        for (int i = 0; i < this.motors.Length; ++i)
+        {
+            state <<= 1;
+            if (this.motors[i])
+            {
+                state |= 1;
+            }
+        }
+        if (state != lastState)
+        {
+            lastState = temp[0] = state;
+            this.stream.Write(temp, 0, 1);
+        }
+    }
+
+    private void ConnectToServer()
+    {
+        this.server = new TcpClient();
+        this.server.Connect("127.0.0.1", 9001);
+        this.stream = this.server.GetStream();
+    }
+
+    private string[] FingerAnimationNames = new string[] {
+        "ThumbCurl",
+        "IndexCurl",
+        "MiddleCurl",
+        "RingCurl",
+        "PinkyCurl"
+    };
+
+    private void UpdateFinger(int index, float value)
+    {
+        if (0 <= index && index < this.fingers.Length)
+        {
+            var f = this.fingers[index];
+            lastFingerValues[index] = value;
+            var animationName = FingerAnimationNames[index];
+            var animation = f[animationName];
+            animation.normalizedTime = 1 - value;
+            animation.speed = 0;
+            f.Play();
+
+            gauntletController.charging = true;
+            for(int i = 0; i < this.fingers.Length && gauntletController.charging; ++i)
+            {
+                gauntletController.charging = lastFingerValues[i] > thresholdValue;
+            }
         }
     }
 }

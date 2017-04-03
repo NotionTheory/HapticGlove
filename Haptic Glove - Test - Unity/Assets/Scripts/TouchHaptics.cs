35043837f614b66bebebb1c124835e9f90854714
Sean T. McBeth <sean.mcbeth@gmail.com>
2017-04-03 00:45:40 -0400

    a crack at sequencable haptics

--- Haptic Glove - Test - Unity/Assets/Scripts/TouchHaptics.cs
+++ Haptic Glove - Test - Unity/Assets/Scripts/TouchHaptics.cs
@@ -5,39 +5,107 @@ using UnityEngine;
 
 public class TouchHaptics : MonoBehaviour
 {
+    private class HapticFrame
+    {
+        public float strength;
+        public float duration;
+
+        public HapticFrame(float strength, float duration)
+        {
+            this.strength = strength;
+            this.duration = duration;
+        }
+    }
+
+    public enum Fingers {
+        LeftThumb,
+        LeftIndex,
+        LeftMiddle,
+        LeftRing,
+        LeftPinky,
+        RightThumb,
+        RightIndex,
+        RightMiddle,
+        RightRing,
+        RightPinky
+    }
+
+    [Range(0f, 1f)]
+    public float PersistentMotorValue = 0f;
+    
+    public Fingers finger = 0;
+
     DeviceServer server;
-    public int fingerIndex = 0;
     Animation anim;
+    Queue<HapticFrame> sequence = new Queue<HapticFrame>();
 
     private void Start()
     {
-        this.server = FindObjectOfType<DeviceServer>();
-        var head = this.transform;
-        while(this.anim == null && head != null)
+        server = FindObjectOfType<DeviceServer>();
+        var head = transform;
+        while(anim == null && head != null)
         {
-            this.anim = head.GetComponent<Animation>();
+            anim = head.GetComponent<Animation>();
             head = head.transform.parent;
         }
     }
 
+    public float MotorValue
+    {
+        set
+        {
+            server.motors[(int)finger] = Mathf.Max(0f, Mathf.Min(1f, value));
+        }
+    }
+
+    public float FingerValue
+    {
+        get
+        {
+            return 1f - Mathf.Pow(server.fingers[(int)finger], 0.5f);
+        }
+    }
+
+    public void Vibrate(float strength, float duration)
+    {
+        sequence.Enqueue(new HapticFrame(strength, duration));
+    }
+
     private void Update()
     {
-        if(this.anim != null)
+        UpdateFingerValue();
+        UpdateMotorValue();
+    }
+
+    private void UpdateFingerValue()
+    {
+        if(anim != null)
         {
-            var state = this.anim[this.anim.name + "Curl"];
+            var state = anim[anim.name + "Curl"];
             if(state != null)
             {
-                state.normalizedTime = 1 - (float)Math.Pow(this.server.fingers[this.fingerIndex], 0.5);
+                state.normalizedTime = FingerValue;
                 state.speed = 0;
-                this.anim.Play();
+                anim.Play();
             }
         }
     }
 
-    //0 = RThumb, 1 = RIndex, 2 = RMiddle, 3 = RRing, 4 = RPinky, 5 = LThumb, 6 = LIndex, 7 = LMiddle, 8 = LRing, 9 = LPinky
-
-    private void OnCollisionEnter(Collision collision)
+    private void UpdateMotorValue()
     {
-        Debug.Log(collision.collider.name);
+        if(sequence.Count == 0)
+        {
+            MotorValue = PersistentMotorValue;
+        }
+        else
+        {
+            var cur = sequence.Peek();
+            MotorValue = cur.strength;
+            cur.duration -= Time.deltaTime;
+            if(cur.duration <= 0)
+            {
+                sequence.Dequeue();
+            }
+        }
     }
 }

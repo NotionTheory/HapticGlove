9fdbd7138e0bc71f6cbdd3b93dd24f38488553a7
Sean T. McBeth <sean.mcbeth@gmail.com>
2017-04-08 04:08:52 -0400

    make dial clickable

--- Touch_Hands_Box_Project/Assets/Scripts/DialBehavior.cs
+++ Touch_Hands_Box_Project/Assets/Scripts/DialBehavior.cs
@@ -2,15 +2,18 @@
 using UnityEngine;
 
 [Serializable]
-public class DialValueChanged: UnityEngine.Events.UnityEvent<int>
+public class DialValueChanged : UnityEngine.Events.UnityEvent<int>
 {
 }
 
 public class DialBehavior : TouchableBehavior
 {
+    [Range(0, 9)]
     public int Value;
+    int lastValue;
+    bool clicked;
     public int NumTicks = 10;
-    
+
     public DialValueChanged ValueChanged;
 
     [Header("Haptic feedback on value change")]
@@ -18,8 +21,8 @@ public class DialBehavior : TouchableBehavior
     public float Strength = 0.5f;
     [Range(0, 1000)]
     public int Length = 100;
-    
-    
+
+
     Transform visibleCylinder, controlCylinder;
     Renderer tab;
     void Start()
@@ -29,13 +32,24 @@ public class DialBehavior : TouchableBehavior
         this.tab = this.visibleCylinder
             .FindChild("Tab")
             .GetComponent<Renderer>();
+        lastValue = -1;
     }
 
     protected override void Update()
     {
         base.Update();
-        var lastValue = Value;
         var euler = this.controlCylinder.localEulerAngles;
+        var recalc = true;
+        if(clicked)
+        {
+            Debug.LogFormat("start: {0} -> {1}", lastValue, Value);
+        }
+        if(Value != lastValue)
+        {
+            Value = Value % 10;
+            euler.y = Value * 360 / NumTicks;
+            recalc = false;
+        }
 
         // Constrain the rotation, because Unity does it in World Space
         // and we want it done in local, model space.
@@ -43,8 +57,16 @@ public class DialBehavior : TouchableBehavior
         euler.z = 0;
         this.controlCylinder.localEulerAngles = euler;
 
-        // Calculate which digit we're pointing at.
-        Value = (int)(euler.y * NumTicks / 360);
+        var pre = Value;
+        if(recalc)
+        {
+            // Calculate which digit we're pointing at.
+            Value = (int)Mathf.Round(euler.y * NumTicks / 360);
+            if(clicked)
+            {
+                Debug.LogFormat("calc: {0} -> {1}", pre, Value);
+            }
+        }
 
         // Chunk the visible dial over there.
         euler.y = Value * 360 / NumTicks;
@@ -52,10 +74,21 @@ public class DialBehavior : TouchableBehavior
 
         tab.material.color = Color.HSVToRGB((float)Value / NumTicks, 1f, 1f);
 
-        if(Value != lastValue && ValueChanged != null)
+        if(Value != lastValue)
         {
             ForFingers((finger) => finger.Vibrate(Strength, Length));
-            ValueChanged.Invoke(Value);
+            if(ValueChanged != null)
+            {
+                ValueChanged.Invoke(Value);
+            }
         }
+        lastValue = Value;
+        clicked = false;
+    }
+
+    private void OnMouseDown()
+    {
+        clicked = true;
+        ++Value;
     }
 }

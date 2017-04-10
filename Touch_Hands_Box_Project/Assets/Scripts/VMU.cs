4b98af4ac85191f83cad26208321d665667e5341
Sean T. McBeth <sean.mcbeth@gmail.com>
2017-04-10 00:41:59 -0400

    auto-generate VMU geometry

--- Touch_Hands_Box_Project/Assets/Scripts/VMU.cs
+++ Touch_Hands_Box_Project/Assets/Scripts/VMU.cs
@@ -2,34 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VMU : MonoBehaviour {
+public class VMU : MonoBehaviour
+{
+    public Transform Prefab;
+    public Vector3 BarScale = new Vector3(0.1f, 4f, 0.1f);
 
-    [Range(0, 5)]
-    public float BarScale = 4;
-    Transform[] bars = new Transform[BeatDetector.BandCount];
-    Vector3 scale = new Vector3(0.1f, 0, 0.1f);
+    List<Transform> bars = new List<Transform>();
 
-	// Use this for initialization
-	void Start ()
-    {
-        for(int i = 0; i < bars.Length; ++i)
-        {
-            bars[i] = this.transform.GetChild(i);
-        }
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
-    
     public void SetVMU(float[] spectrum)
     {
-        for(int i = 0; i < spectrum.Length; ++i)
+        while(bars.Count < spectrum.Length)
         {
-            scale.y = spectrum[i] * BarScale;
-            bars[i].localScale = scale;
+            var bar = Instantiate(Prefab);
+            bar.SetParent(transform);
+            bars.Add(bar);
+        }
+
+        while(bars.Count > spectrum.Length)
+        {
+            var last = bars.Count - 1;
+            var bar = bars[last];
+            bars.RemoveAt(last);
+            Destroy(bar.gameObject);
+        }
+
+        var offset = 0.5f * (bars.Count - 1);
+        for(int i = 0; i < bars.Count; ++i)
+        {
+            var bar = bars[i];
+            var scale = BarScale;
+            scale.y *= spectrum[i];
+            bar.localScale = scale;
+            bar.transform.localPosition = new Vector3(BarScale.x * (i - offset), 0, 0);
         }
     }
 }
